7133b0c0c074063a34b2310c341ebf6f298c8881
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-02-20 16:48:35 -0500

    added tables

--- backend/db/migrations/20230220175301-create-review-image.js
+++ backend/db/migrations/20230220175301-create-review-image.js
@@ -18,7 +18,6 @@ module.exports = {
       },
       url: {
         type: Sequelize.STRING,
-        allowNull: false,
       },
       createdAt: {
         allowNull: false,
--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
   ReviewImage.init(
     {
       reviewId: { type: DataTypes.INTEGER, allowNull: false },
-      url: { type: DataTypes.STRING, allowNull: false },
+      url: { type: DataTypes.STRING },
     },
     {
       sequelize,
