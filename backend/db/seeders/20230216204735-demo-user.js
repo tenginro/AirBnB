95e8b4e80e981a76d0adee5921872604d29b065c
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-02-19 19:15:19 -0500

    updating db

--- backend/db/seeders/20230216204735-demo-user.js
+++ backend/db/seeders/20230216204735-demo-user.js
@@ -27,16 +27,22 @@ module.exports = {
         {
           email: "demo@user.io",
           username: "Demo-lition",
+          firstName: "Demo",
+          lastName: "Lition",
           hashedPassword: bcrypt.hashSync("password"),
         },
         {
           email: "user1@user.io",
           username: "FakeUser1",
+          firstName: "User",
+          lastName: "One",
           hashedPassword: bcrypt.hashSync("password2"),
         },
         {
           email: "user2@user.io",
           username: "FakeUser2",
+          firstName: "Fake",
+          lastName: "Two",
           hashedPassword: bcrypt.hashSync("password3"),
         },
       ],
