e4c0cd8d73bb9824df7c3f8323a5f447bdd825ad
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-02-20 21:21:16 -0500

    finish get all spots locally

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { User, Spot, Review, sequelize } = require("../../db/models");
+const { User, Spot, Review, SpotImage, sequelize } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
@@ -9,6 +9,8 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   const spots = await Spot.findAll();
+
+  let arr = [];
   for (let spot of spots) {
     // console.log("spotid", spot.id);
     let totalRating = await Review.sum("stars", {
@@ -24,11 +26,26 @@ router.get("/", async (req, res) => {
     });
     // console.log(countRating);
     let avgRating = (totalRating / countRating).toFixed(1);
-    console.log(avgRating);
-    spot.avgRating = avgRating;
+    // console.log(avgRating);
+    // spot["avgRating"] = avgRating;
+    // console.log(spot.avgRating);
+    let previewImage = await SpotImage.findOne({
+      where: {
+        spotId: spot.id,
+        preview: true,
+      },
+    });
+    if (previewImage) {
+      //   spot["previewImage"] = previewImage.url;
+      // } else spot["previewImage"] = "no preview image";
+      arr.push({
+        ...spot.toJSON(),
+        avgRating: avgRating,
+        previewImage: previewImage.url,
+      });
+    }
   }
-
-  return res.status(200).json({ Spots: spots });
+  return res.status(200).json({ Spots: arr });
 });
 
 module.exports = router;
