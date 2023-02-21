90a4b327b44264e395f731a5a49ec2d8b2f7a679
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-02-21 13:08:00 -0500

    add restoreUser middleware back to the session.js

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -64,8 +64,9 @@ router.delete("/", (_req, res) => {
 // Restore session user
 // If there is not a session, it will return a JSON with an empty object. To get the session user, connect the restoreUser middleware.
 // Answered: shall i remove restoreUser here since it is applied globally? - Certainly
-// router.get("/", restoreUser, requireAuth, (req, res) => {
-router.get("/", requireAuth, (req, res) => {
+// However, it keeps giving csrf error without the restoreUser middleware - Interesting
+router.get("/", restoreUser, requireAuth, (req, res) => {
+  // router.get("/", requireAuth, (req, res) => {
   const { user } = req;
   if (user) {
     return res.status(200).json({
