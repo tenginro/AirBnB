60283b97d64043001b4b1dab47d378acc85506f9
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-02-21 16:12:44 -0500

    finish get all reviews of current user locally

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -25,47 +25,123 @@ const validateNewReview = [
     .withMessage("Stars must be an integer from 1 to 5"),
   handleValidationErrors,
 ];
+const validateNewReviewImage = [
+  check("url")
+    .exists({ checkFalsy: true })
+    .withMessage("Image url is required"),
+  handleValidationErrors,
+];
 
 // post a new reviewImage
-router.post("/:reviewId/images", requireAuth, async (req, res) => {
-  const reviewId = req.params.reviewId;
-  const { url } = req.body;
-  const review = await Review.findOne({
-    where: {
-      id: reviewId,
-    },
-  });
-  if (!review) {
-    return res.status(404).json({
-      message: "Review couldn't be found",
-      statusCode: 404,
+router.post(
+  "/:reviewId/images",
+  requireAuth,
+  validateNewReviewImage,
+  async (req, res) => {
+    const reviewId = req.params.reviewId;
+    const { url } = req.body;
+    const review = await Review.findOne({
+      where: {
+        id: reviewId,
+      },
     });
-  }
-  //   review must belong to the current user
-  if (parseInt(review.userId) !== parseInt(req.user.id)) {
-    return res.status(403).json({
-      message: "Forbidden",
-      statusCode: 403,
+    if (!review) {
+      return res.status(404).json({
+        message: "Review couldn't be found",
+        statusCode: 404,
+      });
+    }
+    //   review must belong to the current user
+    if (parseInt(review.userId) !== parseInt(req.user.id)) {
+      return res.status(403).json({
+        message: "Forbidden",
+        statusCode: 403,
+      });
+    }
+    const existingReviewImages = await ReviewImage.count({
+      where: {
+        reviewId: reviewId,
+      },
     });
-  }
-  const existingReviewImages = await ReviewImage.count({
-    where: {
+    if (existingReviewImages >= 10) {
+      return res.status(403).json({
+        message: "Maximum number of images for this resource was reached",
+        statusCode: 403,
+      });
+    }
+    const newReviewImage = await ReviewImage.create({
       reviewId: reviewId,
-    },
-  });
-  if (existingReviewImages >= 10) {
-    return res.status(403).json({
-      message: "Maximum number of images for this resource was reached",
-      statusCode: 403,
+      url: url,
+    });
+    return res.status(200).json({
+      id: newReviewImage.id,
+      url: newReviewImage.url,
     });
   }
-  const newReviewImage = await ReviewImage.create({
-    reviewId: reviewId,
-    url: url,
+);
+
+// get all reviews of current user
+router.get("/current", requireAuth, async (req, res) => {
+  const userId = req.user.id;
+  const reviews = await Review.findAll({
+    where: {
+      userId: userId,
+    },
   });
+  let arr = [];
+  for (let i in reviews) {
+    let obj = reviews[i].toJSON();
+    console.log(reviews[i].toJSON());
+    arr.push(obj);
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+      attributes: ["id", "firstName", "lastName"],
+    });
+    const spot = await Spot.findOne({
+      where: {
+        ownerId: userId,
+      },
+      attributes: [
+        "id",
+        "ownerId",
+        "address",
+        "city",
+        "state",
+        "country",
+        "lat",
+        "lng",
+        "name",
+        "price",
+      ],
+    });
+    const previewImage = await SpotImage.findOne({
+      where: {
+        spotId: parseInt(spot.id),
+        preview: true,
+      },
+      attributes: ["url"],
+    });
+    const reviewImages = await ReviewImage.findAll({
+      where: {
+        reviewId: reviews[i].id,
+      },
+      attributes: ["id", "url"],
+    });
+    arr[i].User = user;
+    if (!previewImage) {
+      arr[i].Spot = {
+        ...spot.toJSON(),
+        previewImage: "Preview image is not set yet",
+      };
+    } else {
+      arr[i].Spot = { ...spot.toJSON(), previewImage: previewImage.url };
+    }
+    arr[i].ReviewImage = reviewImages;
+  }
   return res.status(200).json({
-    id: newReviewImage.id,
-    url: newReviewImage.url,
+    Reviews: arr,
   });
 });
 
