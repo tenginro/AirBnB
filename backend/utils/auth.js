aeff77a048b1f34102bb3e495e880880749a7e3c
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-02-23 11:18:47 -0500

    remove auth err.errors message

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -60,7 +60,7 @@ const requireAuth = function (req, _res, next) {
 
   const err = new Error("Authentication required");
   err.title = "Authentication required";
-  err.errors = { message: "Authentication required" };
+  // err.errors = { message: "Authentication required" };
   err.status = 401;
   return next(err);
 };
