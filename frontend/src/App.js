af85d0afaac87883b9cb8110bcf0e5109cfe275c
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-05-25 15:11:02 +0800

    add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import EditSpotFormWrapper from "./components/EditSpot";
 import UserReviews from "./components/UserReviews";
 import UserBookings from "./components/UserBookings";
 import NotFound from "./components/NotFound";
+import Footer from "./components/Footer";
 
 function App() {
   const dispatch = useDispatch();
@@ -54,6 +55,7 @@ function App() {
           </Route>
         </Switch>
       )}
+      <Footer />
     </>
   );
 }
