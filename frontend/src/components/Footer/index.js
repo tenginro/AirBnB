7f1cd1885a2b24f3a2f2d5c456f7ba45d2e9543a
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-06-26 18:23:21 -0400

    update footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -3,61 +3,39 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer id="footer">
-      <div id="toAlign">
-        <div id="footerPart">
-          <div className="footerSection">
-            <h3>About Me</h3>
-            <div style={{ cursor: "pointer" }}>
-              <a
-                href="https://github.com/tenginro"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <i className="fa-brands fa-github fa-2x"></i> GitHub
-              </a>
-            </div>
-            <div style={{ cursor: "pointer" }}>
-              <a
-                href="https://www.linkedin.com/in/luotengzhong/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <i className="fa-brands fa-linkedin fa-2x"></i> LinkedIn
-              </a>
-            </div>
-            <div style={{ cursor: "pointer" }}>
-              <a
-                href="http://tenginro.me/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <i className="fa-regular fa-user fa-2x"></i> Portfolio
-              </a>
-            </div>
-          </div>
-          <div className="footerSection">
-            <h3>Technologies Used</h3>
-            <div>Javascript</div>
-            <div>React</div>
-            <div>Redux</div>
-            <div>Express</div>
-            <div>PostgreSQL</div>
-          </div>
-          <div className="footerSection">
-            <h3>Additional Tech Used</h3>
-            <div>AWS S3</div>
-            <div>Google Maps API</div>
-          </div>
-          <div className="footerSection">
-            <h3>Language</h3>
-            <div>English</div>
-            <h3>Country</h3>
-            <div>United States</div>
-          </div>
-        </div>
+      <div className="footerContainer">
         <div className="copyrightLine">
           Copyright © 2023 HereBnB Inc. HereBnB, HereBnB logo and related marks
           are registered trademarks of HereBnB.
+        </div>
+        <div className="footerLinks">
+          <div style={{ cursor: "pointer" }}>
+            <a
+              href="https://github.com/tenginro"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <i className="fa-brands fa-github"></i> GitHub
+            </a>
+          </div>
+          <div style={{ cursor: "pointer" }}>
+            <a
+              href="https://www.linkedin.com/in/luotengzhong/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <i className="fa-brands fa-linkedin"></i> LinkedIn
+            </a>
+          </div>
+          <div style={{ cursor: "pointer" }}>
+            <a
+              href="http://tenginro.me/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <i className="fa-regular fa-user"></i> Portfolio
+            </a>
+          </div>
         </div>
       </div>
     </footer>
